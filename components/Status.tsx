01482f82eafd9370e91d8c5b7885bd13e527c307
tlylt <liu_yongliang@hotmail.com>
2021-10-06 00:10:30 +0800

    port over some components

---
+++ components/Status.tsx
@@ -0,0 +1,86 @@
+import Meter from './Meter'
+
+export default function Status() {
+  return (
+    <div id="header" className="flex items-center justify-around text-center">
+      <Meter name="date">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-calendar"
+          width="44"
+          height="44"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#2c3e50"
+          fill="#00abfb"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <rect x="4" y="5" width="16" height="16" rx="2" />
+          <line x1="16" y1="3" x2="16" y2="7" />
+          <line x1="8" y1="3" x2="8" y2="7" />
+          <line x1="4" y1="11" x2="20" y2="11" />
+          <line x1="11" y1="15" x2="12" y2="15" />
+          <line x1="12" y1="15" x2="12" y2="18" />
+        </svg>
+      </Meter>
+      <Meter name="wealth">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-coin"
+          width="44"
+          height="44"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#2c3e50"
+          fill="#ffbf00"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <circle cx="12" cy="12" r="9" />
+          <path d="M14.8 9a2 2 0 0 0 -1.8 -1h-2a2 2 0 0 0 0 4h2a2 2 0 0 1 0 4h-2a2 2 0 0 1 -1.8 -1" />
+          <path d="M12 6v2m0 8v2" />
+        </svg>
+      </Meter>
+      <Meter name="health" hasBar={true}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-heart"
+          width="44"
+          height="44"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#2c3e50"
+          fill="red"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+        </svg>
+      </Meter>
+      <Meter name="social" hasBar={true}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="icon icon-tabler icon-tabler-friends"
+          width="44"
+          height="44"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="#2c3e50"
+          fill="#00bfd8"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <circle cx="7" cy="5" r="2" />
+          <path d="M5 22v-5l-1 -1v-4a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v4l-1 1v5" />
+          <circle cx="17" cy="5" r="2" />
+          <path d="M15 22v-4h-2l2 -6a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1l2 6h-2v4" />
+        </svg>
+      </Meter>
+    </div>
+  )
+}
---
+++ pages/demo.tsx
@@ -0,0 +1,56 @@
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import { create } from 'superstruct'
+import { GameSchema } from '../schema/game'
+import { Game, makeGame } from '../lib/game'
+import mvpJson from '../schema/mvp.json'
+import GameDisplay from '../components/GameDisplay'
+
+const Demo: React.FC = () => {
+  const [game, setGame] = useState<Game | undefined>()
+
+  useEffect(() => {
+    let schema
+    try {
+      schema = create(JSON.parse(JSON.stringify(mvpJson, null, 2)), GameSchema)
+    } catch (e) {
+      console.log({ e })
+      setGame(undefined)
+      return
+    }
+    setGame(makeGame(schema))
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>In My Shoes</title>
+        <meta name="description" content="In Their Shoes | Soristic" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#01948e" />
+        <meta name="title" content="In My Shoes | Soristic" />
+        <meta
+          name="Description"
+          property="og:description"
+          content="Simulation game, developed by Soristic"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="In My Shoes | Soristic" />
+        <meta
+          property="og:description"
+          content="Simulation game, developed by Soristic"
+        />
+        <meta name="author" content="Soristic" />
+      </Head>
+      <main className="flex flex-col items-center py-5 space-y-5">
+        {game && <GameDisplay game={game} />}
+      </main>
+    </>
+  )
+}
+
+export default Demo
