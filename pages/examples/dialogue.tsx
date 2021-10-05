c8fc1ce10360184a70c1581875a50926eeb67242
wltan <53135010+wltan@users.noreply.github.com>
2021-10-05 21:59:02 +0800

    Import GameDisplay into example

--- pages/examples/dialogue.tsx
+++ pages/examples/dialogue.tsx
@@ -1,28 +1,14 @@
 import React, {
   ChangeEventHandler,
-  useCallback,
   useEffect,
   useState,
 } from 'react'
 import { create } from 'superstruct'
-import Dialogue from '../../components/Dialogue'
 import { useDebounce } from '../../hooks/useDebounce'
 import { Game, makeGame } from '../../lib/game'
-import { Scene } from '../../lib/scene'
 import { GameSchema } from '../../schema/game'
-import { useStore } from '../../stores/store'
 import exampleJson from '../../schema/example-dialogue.json'
-import { useAfterInteractionCallback } from '../../hooks/useAfterInteractionCallback'
-import { useRunCleanupFnsOnUnmount } from '../../hooks/useRunCleanupFnsOnUnmount'
-import { Action } from '../../lib/actions'
-import {
-  Narration,
-  Clickable,
-  isClickableText,
-  isClickableImg,
-  Image,
-} from '../../lib/elements'
-import ClickableGroup from '../../components/ClickableGroup'
+import GameDisplay from '../../components/GameDisplay'
 
 const SchemaInput: React.FC = () => {
   const [inputSchema, setInputSchema] = useState<string>('')
@@ -75,142 +61,3 @@ const SchemaInput: React.FC = () => {
 }
 
 export default SchemaInput
-
-type SceneProps = {
-  scene: Scene
-}
-
-const SceneDisplay: React.FC<SceneProps> = ({ scene }) => {
-  const executeActions = useStore((state) => state.executeActions)
-  const resetScene = useStore((state) => state.resetScene)
-
-  useEffect(() => {
-    resetScene(scene.id)
-  }, [resetScene, scene.id])
-
-  useEffect(() => {
-    return executeActions(...scene.intro)
-  }, [scene.intro, executeActions])
-
-  return (
-    <div className="relative w-full h-full overflow-hidden">
-      <img
-        className="m-auto w-full h-full object-cover"
-        src={scene.background}
-        alt={scene.backgroundAltText ?? ''}
-      />
-      {scene.narrations.map((narration) => (
-        <TempNarration {...narration} key={narration.name} />
-      ))}
-      {scene.dialogues.map((dialogue) => (
-        <Dialogue {...dialogue} key={dialogue.name} />
-      ))}
-      {scene.images.map((image) => (
-        <TempImage {...image} key={image.name}></TempImage>
-      ))}
-      {scene.clickables.map((clickableGroup) => (
-        <ClickableGroup
-          sceneId={scene.id}
-          {...clickableGroup}
-          key={clickableGroup.name}
-        />
-      ))}
-    </div>
-  )
-}
-
-type GameProps = {
-  game?: Game
-}
-
-const GameDisplay: React.FC<GameProps> = ({ game: newGame }) => {
-  const game = useStore((state) => state.game)
-  const loadGame = useStore((state) => state.loadGame)
-
-  useEffect(() => {
-    if (newGame) {
-      loadGame(newGame)
-    }
-  }, [newGame, loadGame])
-
-  return (
-    <div className="flex flex-col items-center my-2 w-full space-y-2">
-      {game && (
-        <div className="text-gray-900 text-2xl font-semibold">{game.name}</div>
-      )}
-      <div className="w-[80vw] h-[45vw] border shadow">
-        {game.scenes.map((scene, idx) => {
-          if (game?.currentSceneId === scene.id) {
-            return <SceneDisplay scene={scene} key={idx} />
-          }
-        })}
-      </div>
-    </div>
-  )
-}
-
-const TempNarration: React.FC<Narration> = ({
-  shown,
-  text,
-  position,
-  afterInteractionCallback,
-}) => {
-  const handleInteraction = useAfterInteractionCallback(
-    afterInteractionCallback
-  )
-
-  if (!shown) {
-    return null
-  }
-
-  return (
-    <div
-      className="positioned h-max absolute m-auto px-4 py-3 w-max bg-gray-100 border border-gray-700 rounded"
-      onClick={handleInteraction}
-    >
-      <span className="text-md text-gray-900 font-semibold">{text}</span>
-      <style jsx>{`
-        .positioned {
-          top: ${position.top || '10%'};
-          right: ${position.right || '0px'};
-          left: ${position.left || '0px'};
-          bottom: ${position.bottom || 'unset'};
-        }
-      `}</style>
-    </div>
-  )
-}
-
-const TempImage: React.FC<Image> = ({
-  shown,
-  src,
-  altText,
-  position,
-  afterInteractionCallback,
-}) => {
-  const handleInteraction = useAfterInteractionCallback(
-    afterInteractionCallback
-  )
-
-  if (!shown) {
-    return null
-  }
-
-  return (
-    <div className="positioned absolute w-1/5" onClick={handleInteraction}>
-      <img
-        src={src}
-        alt={altText || ''}
-        className="object-cover mix-blend-lighten"
-      />
-      <style jsx>{`
-        .positioned {
-          top: ${position.top || 'unset'};
-          right: ${position.right || '0px'};
-          left: ${position.left || '0px'};
-          bottom: ${position.bottom || '5%'};
-        }
-      `}</style>
-    </div>
-  )
-}
