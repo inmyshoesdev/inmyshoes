d2f997937a4ca0c85e969a26fdc8530a8bb26e7c
Huang Weijie <h_weijie@yahoo.com>
2021-10-08 02:55:15 +0800

    feat: updated examples

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,9 @@ const Home: NextPage = () => {
         <Link href="/examples/dialogue">
           <a className="text-blue-600 cursor-pointer">dialogue</a>
         </Link>
+        <Link href="/examples/clickable">
+          <a className="text-blue-600 cursor-pointer">clickable</a>
+        </Link>
         <Link href="/examples/narration">
           <a className="text-blue-600">narration</a>
         </Link>
