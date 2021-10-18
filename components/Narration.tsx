55ed2f0bfae591393116aa682af1c691f1f0e697
Tan Wei Liang <53135010+wltan@users.noreply.github.com>
2021-10-19 01:06:33 +0800

    Update intro sequence (#44)
    * Update intro sequence
    
    * Add custom background for narration box
    
    * Update speech background image
    
    * Fix button color

--- components/Narration.tsx
+++ components/Narration.tsx
@@ -50,6 +50,7 @@ const Narration: React.FC<NarrationProps> = ({
       className="absolute top-0 flex items-center justify-evenly w-full h-full"
     >
       <DialogueBox
+        image="./images/narration-background.png"
         position={position}
         dimension={dimension}
         bodyStyle={{
