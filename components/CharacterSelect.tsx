095df5600c92281615d19041e1c8c71eed94620a
Huang Weijie <h_weijie@yahoo.com>
2021-10-28 20:59:02 +0800

    feat: update character select screen background

--- components/CharacterSelect.tsx
+++ components/CharacterSelect.tsx
@@ -1,8 +1,14 @@
 import { MainCharacter } from '../lib/character'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { A11y, EffectCube, Mousewheel, Pagination } from 'swiper'
+import { A11y, EffectCards, Navigation, Mousewheel } from 'swiper'
 import { useState } from 'react'
+import Image from 'next/image'
 import { useStore } from '../stores/store'
+import Tape from './Tape'
+import paperBg from '../public/images/paper-bg.jpg'
+import 'swiper/css'
+import 'swiper/css/navigation'
+
 function CharacterSelect({
   characterSelected,
   mainCharacters,
@@ -19,66 +25,105 @@ function CharacterSelect({
     setCharacterSelected(true)
   }
   return (
-    <>
-      <div
-        className={`${
-          characterSelected ? 'hidden' : ''
-        } bg relative w-auto mx-auto flex flex-col items-center justify-evenly`}
-        style={{
-          aspectRatio: '16/9',
-        }}
-      >
-        <h1 className="px-2 py-1 text-white text-xs bg-gray-700 rounded opacity-95 select-none sm:text-sm md:px-3 md:py-2 md:text-base lg:text-lg xl:text-xl">
+    <div
+      className={`${
+        characterSelected ? 'hidden' : ''
+      } bg relative w-auto mx-auto flex flex-col items-center justify-evenly`}
+      style={{
+        aspectRatio: '16/9',
+      }}
+    >
+      <Image
+        className="absolute z-0 inset-0"
+        src={paperBg}
+        alt="background of corkboard"
+        layout="fill"
+      />
+      <h1 className="px-2 py-1 bg-gray-700 border border-gray-900 rounded-handdrawn opacity-90 select-none md:px-3 md:py-2 md:border-2">
+        <span className="text-white text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
           Character Selection
-        </h1>
-        <div className="w-1/2 h-1/2 text-xs sm:w-full sm:text-base md:h-2/3">
-          <Swiper
-            modules={[A11y, Mousewheel, EffectCube, Pagination]}
-            effect="cube"
-            mousewheel={true}
-            pagination={true}
-            className="w-[25vw] h-full"
-            onSlideChange={(swiper) => {
-              setCharacterIndex(swiper.activeIndex)
-            }}
-          >
-            {mainCharacters.map((character) => (
-              <SwiperSlide
-                key={character.name}
-                className="flex items-center justify-evenly bg-gray-100 select-none md:block"
-              >
-                <div className="my-2 h-full md:mb-0 md:h-4/5">
-                  <img
-                    src={character.images.default}
-                    alt={character.name}
-                    className="w-full h-full object-contain"
-                  />
-                </div>
-                <div className="text-md grid place-items-center -mt-4 h-1/5 md:text-xl lg:text-2xl xl:text-3xl">
-                  <span className="text-center text-gray-800 italic font-medium">
-                    {character.name}
-                  </span>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-        <div className="z-10 mt-1 text-center text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl">
-          <button
-            className="px-4 py-1 font-medium bg-green-400 hover:bg-green-500 rounded-md focus:outline-none shadow-md cursor-pointer md:px-6 md:py-2"
-            onClick={finishSelection}
-          >
-            Go
-          </button>
-        </div>
-      </div>
+        </span>
+      </h1>
       <style jsx>{`
-        .bg {
-          background-color: #f6cfcf;
-          background-image: url("data:image/svg+xml,%3Csvg width='42' height='44' viewBox='0 0 42 44' xmlns='http://www.w3.org/2000/svg'%3E%3Cg id='Page-1' fill='none' fill-rule='evenodd'%3E%3Cg id='brick-wall' fill='%23891818' fill-opacity='0.4'%3E%3Cpath d='M0 0h42v44H0V0zm1 1h40v20H1V1zM0 23h20v20H0V23zm22 0h20v20H22V23z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+        .swiper-parent {
+          --swiper-navigation-color: #1f2937;
+          --swiper-navigation-size: 22px;
+        }
+
+        @media (min-width: 640px) {
+          .swiper-parent {
+            --swiper-navigation-size: 33px;
+          }
+        }
+
+        @media (min-width: 768px) {
+          .swiper-parent {
+            --swiper-navigation-size: 44px;
+          }
+        }
+
+        @media (min-width: 1280px) {
+          .swiper-parent {
+            --swiper-navigation-size: 55px;
+          }
+        }
+
+        .swiper-parent :global(.swiper-button-next) {
+          right: -25%;
+        }
+
+        .swiper-parent :global(.swiper-button-prev) {
+          left: -25%;
         }
       `}</style>
-    </>
+      <div className="swiper-parent w-1/2 h-1/2 text-xs sm:w-full sm:text-base md:h-2/3">
+        <Swiper
+          modules={[A11y, Mousewheel, EffectCards, Navigation]}
+          effect="cards"
+          navigation={true}
+          mousewheel={true}
+          grabCursor={true}
+          className="w-[25vw] h-full"
+          onSlideChange={(swiper) => {
+            setCharacterIndex(swiper.activeIndex)
+          }}
+        >
+          {mainCharacters.map((character) => (
+            <SwiperSlide
+              key={character.name}
+              className="flex items-center justify-evenly p-2 bg-white shadow-md select-none md:block md:p-4 md:pb-0"
+            >
+              <div className="relative flex-grow h-full bg-gray-100 shadow-sm md:mb-0 md:h-4/5">
+                <div className="absolute -left-1 top-0 md:-left-2 xl:top-1">
+                  <Tape rotation={-45}></Tape>
+                </div>
+                <div className="absolute -right-1 top-0 md:-right-2 xl:top-1">
+                  <Tape rotation={39}></Tape>
+                </div>
+                <img
+                  src={character.images.default}
+                  alt={character.name}
+                  className="w-full h-full object-contain"
+                />
+              </div>
+              <div className="grid place-items-center ml-2 h-1/5 text-sm md:-mt-1 md:text-base lg:text-xl xl:text-2xl">
+                <span className="text-center text-gray-800 italic font-medium">
+                  {character.name}
+                </span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="z-10 mt-1 text-center font-handwritten text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl">
+        <button
+          className="hover:bg-gray-100/25 px-4 py-1 font-medium border-2 border-gray-900 rounded-handdrawn focus:outline-none cursor-pointer md:px-6 md:py-2 md:border-3"
+          onClick={finishSelection}
+        >
+          Go
+        </button>
+      </div>
+    </div>
   )
 }
 
